fdac1bc143386e107637131fd3f0b04af82d291b
Cruz Lopez <cml231@txstate.edu>
2024-03-04 20:52:50 -0600

    created game.py

---
+++ model/game.py
@@ -0,0 +1,34 @@
+
+
+class Game():
+    def __init__(self, window, players, deck) -> None:
+        self.window = window
+        self.players_list = players
+        self.discard_pile= []
+        self.draw_pile = deck
+        self.current_direction = 1
+        
+    
+    def check_direction():
+        pass
+    
+    def change_direction():
+        pass
+    
+    def check_game_end():
+        pass
+    
+    def determine_next_player():
+        pass
+    
+    def play_card(self,player,card):
+        pass
+    
+    def check_last_card_played():
+        pass
+    
+    def set_current_color(self,color):
+        pass
+    
+    def start_game():
+        pass
