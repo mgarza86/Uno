dd1fc2180a85b48891bd9f90278afa949c7f830f
Melanie Garza <mmg252@txstate.edu>
2024-03-15 16:57:17 -0500

    added toggle sfx button

--- view/settings.py
+++ view/settings.py
@@ -5,6 +5,7 @@ import pyghelpers
 # constants
 window_width = 800
 window_height = 600
+yellow = (255, 255, 0)
 
 # settings scene class
 class SettingsScene(pyghelpers.Scene):
@@ -12,12 +13,22 @@ class SettingsScene(pyghelpers.Scene):
         super().__init__()
         self.window = window
 
-        # AI Difficulty Buttons
+        # AI difficulty buttons
         self.title = pygwidgets.DisplayText(window, (350, 130), "Difficulty", fontSize=40, textColor="white")
-        self.easy_button = pygwidgets.TextButton(window, (200, 200), "Easy")
-        self.medium_button = pygwidgets.TextButton(window, (350, 200), "Medium")
-        self.hard_button = pygwidgets.TextButton(window, (500, 200), "Hard")
+        self.easy_button = pygwidgets.TextButton(window, (200, 200), "Easy", upColor=yellow, overColor=yellow, downColor=yellow)
+        self.medium_button = pygwidgets.TextButton(window, (350, 200), "Medium", upColor=yellow, overColor=yellow, downColor=yellow)
+        self.hard_button = pygwidgets.TextButton(window, (500, 200), "Hard", upColor=yellow, overColor=yellow, downColor=yellow)
         
+        # SFX toggle button
+        self.sfx_on = True
+        self.sfx_toggle = pygwidgets.TextButton(window, (300, 320), "SFX: On", width=200, height=40)
+
+    
+    def toggle_sfx_button(self):
+        # toggles the state and updates the button label
+        self.sfx_on = not self.sfx_on
+        new_label = "SFX: " + ("On" if self.sfx_on else "Off")
+        self.sfx_toggle = pygwidgets.TextButton(self.window, (300, 320), new_label, width=200, height=40)
 
     def handleInputs(self, events, keyPressedList):
         for event in events:  # iterate over each event in the list of events
@@ -28,13 +39,17 @@ class SettingsScene(pyghelpers.Scene):
                 print("Medium difficulty selected")
             elif self.hard_button.handleEvent(event):
                 print("Hard difficulty selected")
-
+            elif self.sfx_toggle.handleEvent(event):
+                self.toggle_sfx_button()  # Update SFX button label
+                print("SFX " + ("on" if self.sfx_on else "off"))
+                
     def draw(self):
         self.window.fill((255, 0, 0))  # red bckgd
         self.title.draw()
         self.easy_button.draw()
         self.medium_button.draw()
         self.hard_button.draw()
+        self.sfx_toggle.draw()
 
 def main():
     pygame.init()
