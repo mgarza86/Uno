6704a91413308aceb7af3700e53b40f20f4d5c2c
Melanie Garza <mmg252@txstate.edu>
2024-03-15 13:42:26 -0500

    initiated each scene

--- scene_manager.py
+++ scene_manager.py
@@ -12,6 +12,17 @@ window_width = 640
 window_height = 480
 frames_per_second = 60
 
+def main():
+    pygame.init()
+    window = pygame.display.set_mode((window_width, window_height))
+    pygame.display.set_caption("Py-UNO")
+
+    # scene creation: instantiating each scene
+    main_menu_scene = MainMenuScene(window)
+    settings_scene = SettingsScene(window)
+    single_player_setup_scene = SinglePlayerSetupScene(window)
+    multi_player_lobby_scene = MultiPlayerLobbyScene(window)
+
 
     
 if __name__ == "__main__":
