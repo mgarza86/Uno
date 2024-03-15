5778b32b18be8e093b23c66b632424e51cabce72
Melanie Garza <mmg252@txstate.edu>
2024-03-15 13:46:47 -0500

    scene management, instance of pyghelpers.SceneMgr & run method of SceneMgr to start

--- scene_manager.py
+++ scene_manager.py
@@ -23,7 +23,19 @@ def main():
     single_player_setup_scene = SinglePlayerSetupScene(window)
     multi_player_lobby_scene = MultiPlayerLobbyScene(window)
 
+    # scene management: store scenes in a dictionary
+    scenes_dict = {
+        'main_menu': main_menu_scene,
+        'settings': settings_scene,
+        'single_player_setup': single_player_setup_scene,
+        'multi_player_lobby': multi_player_lobby_scene
+    }
 
+    # create an instance of pyghelpers.SceneMgr with the dictionary of scenes and frame rate
+    scene_manager = pyghelpers.SceneMgr(scenes_dict, frames_per_second)
+
+    # utilize the run method of SceneMgr to start and manage the application
+    scene_manager.run()
     
 if __name__ == "__main__":
     main()
