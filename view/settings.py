f7e534ad513afeb66ee972a8fe25914820c6ff0a
Melanie Garza <mmg252@txstate.edu>
2024-03-15 17:30:24 -0500

    added back to main menu button + minor fixes

--- view/settings.py
+++ view/settings.py
@@ -15,33 +15,36 @@ class SettingsScene(pyghelpers.Scene):
         self.main_title = pygwidgets.DisplayText(window, (305, 40), "Settings", fontSize=75, textColor="black")
 
         # AI difficulty buttons
-        self.title = pygwidgets.DisplayText(window, (345, 150), "Difficulty", fontSize=40, textColor="white")
+        self.title = pygwidgets.DisplayText(window, (345, 150), "Difficulty", fontSize=40, textColor="black")
         self.easy_button = pygwidgets.TextButton(window, (200, 200), "Easy", upColor=yellow, overColor=yellow, downColor=yellow)
         self.medium_button = pygwidgets.TextButton(window, (350, 200), "Medium", upColor=yellow, overColor=yellow, downColor=yellow)
         self.hard_button = pygwidgets.TextButton(window, (500, 200), "Hard", upColor=yellow, overColor=yellow, downColor=yellow)
         
         # SFX toggle button
-        self.sfx_title = pygwidgets.DisplayText(window, (325, 290), "SFX Volume", fontSize=40, textColor="white")
+        self.sfx_title = pygwidgets.DisplayText(window, (325, 290), "SFX Volume", fontSize=40, textColor="black")
         self.sfx_on = True
         self.sfx_toggle = pygwidgets.TextButton(window, (300, 340), "SFX: On", width=200, height=40, upColor=yellow, overColor=yellow, downColor=yellow)
         
         # Music toggle button
-        self.music_title = pygwidgets.DisplayText(window, (310, 430), "Music Volume", fontSize=40, textColor="white")
+        self.music_title = pygwidgets.DisplayText(window, (310, 430), "Music Volume", fontSize=40, textColor="black")
         self.music_on = True
         self.music_toggle = pygwidgets.TextButton(window, (300, 480), "Music: On", width=200, height=40, upColor=yellow, overColor=yellow, downColor=yellow)
+        
+        # Back to Main Menu button
+        self.backButton = pygwidgets.TextButton(window, (30, 550), "Back to Main Menu", upColor=yellow, overColor=yellow, downColor=yellow)
 
     
     def toggle_sfx_button(self):
         # toggles the state and updates the button label
         self.sfx_on = not self.sfx_on
         new_label = "SFX: " + ("On" if self.sfx_on else "Off")
-        self.sfx_toggle = pygwidgets.TextButton(self.window, (300, 340), new_label, width=200, height=40)
+        self.sfx_toggle = pygwidgets.TextButton(self.window, (300, 340), new_label, width=200, height=40, upColor=yellow, overColor=yellow, downColor=yellow)
         
     def toggle_music_button(self):
         # toggles the state and updates the button label
         self.music_on = not self.music_on
         new_label = "Music: " + ("On" if self.music_on else "Off")
-        self.music_toggle = pygwidgets.TextButton(self.window, (300, 480), new_label, width=200, height=40)
+        self.music_toggle = pygwidgets.TextButton(self.window, (300, 480), new_label, width=200, height=40, upColor=yellow, overColor=yellow, downColor=yellow)
 
     def handleInputs(self, events, keyPressedList):
         for event in events:  # iterate over each event in the list of events
@@ -58,6 +61,8 @@ class SettingsScene(pyghelpers.Scene):
             elif self.music_toggle.handleEvent(event):
                 self.toggle_music_button()  # update Music button label
                 print("Music " + ("on" if self.music_on else "off"))
+            elif self.backButton.handleEvent(event):
+                self.goToScene('main_menu')
                 
                 
     def draw(self):
@@ -71,6 +76,7 @@ class SettingsScene(pyghelpers.Scene):
         self.sfx_toggle.draw()
         self.music_title.draw()
         self.music_toggle.draw()
+        self.backButton.draw()
 
 def main():
     pygame.init()
