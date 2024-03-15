5e451098f17330880723801a78895a32e1681321
Melanie Garza <mmg252@txstate.edu>
2024-03-15 12:22:32 -0500

    importing scenes in file

---
+++ scene_manager.py
@@ -0,0 +1,9 @@
+import pygame
+import pyghelpers
+
+# importing scenes from the view folder
+from view import mainMenu as MainMenuScene 
+from view import settings as SettingsScene
+from view import single_player as SinglePlayerSetupScene
+from view import multi_player as MultiPlayerLobbyScene
+
