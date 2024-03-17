fb2cd0752beb727176894b88a3e00cc9c85a7ca4
Melanie Garza <mmg252@txstate.edu>
2024-03-17 17:51:50 -0500

    comment fixes on scene manager file

--- scene_manager.py
+++ scene_manager.py
@@ -16,13 +16,13 @@ pygame.init()
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Py-UNO")
 
-    # scene creation: instantiating each scene
+# scene creation: instantiating each scene
 main_menu_scene = MainMenuScene(window)
 settings_scene = SettingsScene(window)
 single_player_scene = SinglePlayerSetupScene(window)
 multi_player_scene = MultiPlayerLobbyScene(window)
 
-    # scene management: store scenes in a dictionary
+# scene management: store scenes in a dictionary
 scenes_dict = {
         'main_menu': main_menu_scene,
         'settings': settings_scene,
@@ -30,8 +30,8 @@ scenes_dict = {
         'multi_player_lobby': multi_player_scene
     }
 
-    # create an instance of pyghelpers.SceneMgr with the dictionary of scenes and frame rate
+# create an instance of pyghelpers.SceneMgr with the dictionary of scenes and frame rate
 scene_manager = pyghelpers.SceneMgr(scenes_dict, frames_per_second)
 
-    # utilize the run method of SceneMgr to start and manage the application
+# utilize the run method of SceneMgr to start and manage the application
 scene_manager.run()
