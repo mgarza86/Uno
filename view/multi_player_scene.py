0b14b9d5beafd95a8d5e7d84f9ac2eab7e81eee4
Melanie Garza <mmg252@txstate.edu>
2024-03-17 16:36:56 -0500

    initiating class

--- view/multi_player_scene.py
+++ view/multi_player_scene.py
@@ -1,3 +1,9 @@
 import pygame
 import pygwidgets
 import pyghelpers
+
+
+class MultiPlayerLobbyScene(pyghelpers.Scene):
+    def __init__(self, window):
+        super().__init__()
+        self.window = window
