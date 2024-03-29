f793f013829c7254996fbb0c3a4807548c08f0a7
Cruz Lopez <cml231@txstate.edu>
2024-03-29 16:31:41 -0500

    added lobby class

---
+++ lobby.py
@@ -0,0 +1,17 @@
+class lobby:
+    def __init__(self, lobby_name, game):
+        self.lobby_name = lobby_name
+        
+    def broadcast_current_color(self, color):
+        pass
+    
+    def broadcast_current_value(self, value):
+        pass
+    
+    def broadcast_current_direction(self, direction):
+        pass
+    
+    def broadcast_player_list(self, player_list):
+        pass
+    
+    
