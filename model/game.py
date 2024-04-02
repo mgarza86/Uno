c8632467ed4fdb16527fc92a652f2ad338d6fe17
Cruz Lopez <cml231@txstate.edu>
2024-04-01 23:55:15 -0500

    created gameview and playerview

--- model/game.py
+++ model/game.py
@@ -31,41 +31,7 @@ class Game():
                 return True
         #player.draw_card(self.draw_pile)
         #print(player, " drew a card")    
-        return False
-    
-    def orientate_player(self):
-        for i in range(len(self.players_list)):
-            if i == 1:
-                for card in self.players_list[i].hand:
-                    card.flip_vertical()
-    
-    def rotate_player_hands(self, players):
-        num_players = len(players)
-        
-        # For two players
-        if num_players == 2:
-            # Apply 180 degrees rotation to the second player's hand
-            players[1].set_angle(180)
-            players[1].rotate_hand(180)
-            
-        # For three players
-        elif num_players == 3:
-            players[1].set_angle(90)
-            players[2].set_angle(180)
-            
-        # For four players
-        elif num_players == 4:
-            players[1].set_angle(90)
-            players[2].set_angle(180)
-            players[3].set_angle(270)
-                
-    def draw(self):
-        if len(self.discard_pile) != 0:
-            self.discard_pile[0].set_centered_location((self.window_width/2,self.window_height/2))
-            self.discard_pile[0].draw()
-        
-        for i in range(len(self.players_list)):
-            self.players_list[i].draw()            
+        return False         
         
     def discard(self, discard_pile, new_card):
         discard_pile.insert(0,new_card)
--- model/player.py
+++ model/player.py
@@ -4,8 +4,8 @@ import pygwidgets
 from model.card import Card
 from model.deck import Deck
 class Player():
-    def __init__(self, window ,name) -> None:
-        self.window = window
+    def __init__(self, name) -> None:
+        #self.window = window
         self.name = name
         self.hand = []
         self.angle = 0
@@ -41,121 +41,6 @@ class Player():
             return True
         #print("card not playable")
         return False
-    
-    def set_angle(self, angle):
-        self.angle = angle
-    
-    def rotate_hand(self, angle):
-        for card in self.hand:
-            card.rotate_card(angle)
-        #self.angle = angle
-    
-    def scale_hand(self, scale):
-        for i in range(len(self.hand)):
-            self.hand[i].scale_card(scale)
-   
-    def draw(self):
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        if isinstance(self,AIPlayer):
-            overlap_amount = 80    
-        else:
-            overlap_amount = 30
-        hovered_index = None
-
-        vertical_layout = self.angle == 90 or self.angle == 270
-
-        for i, card in enumerate(self.hand):
-            if card.get_collide_point(mouse_x, mouse_y):
-                hovered_index = i
-                break  
-
-        for i, card in enumerate(self.hand):
-            if i != hovered_index:
-                if i == 0:
-                    card_location = self.set_card_on_center(card)
-                else:
-                    card_location = self.next_card_location(self.hand[i-1], overlap=overlap_amount, vertical=vertical_layout)
-                card.set_location(card_location)
-                
-                if isinstance(self, AIPlayer):
-                    card.conceal()        
-                elif isinstance(self, Player):
-                    card.reveal()
-                card.set_scale(60)
-                card.draw()
-
-        if hovered_index is not None:
-            if vertical_layout:
-                card_location = self.next_card_location(self.hand[hovered_index-1], overlap=overlap_amount, vertical=vertical_layout) if hovered_index > 0 else self.set_card_on_center(self.hand[hovered_index])
-                self.hand[hovered_index].set_location(card_location)
-            self.hand[hovered_index].reveal()
-            self.hand[hovered_index].set_scale(60)
-            self.hand[hovered_index].draw()
-
-    def calculate_hand_width(self, overlap):
-        if not self.hand:
-            return 0
-        total_width = sum(card.get_size()[0] for card in self.hand) - overlap * (len(self.hand) - 1)
-        return total_width
-    
-    def adjust_first_card_position(self, overlap):
-        window_width, _ = self.window.get_size()
-        hand_width = self.calculate_hand_width(overlap)
-
-        first_card_x = (window_width - hand_width) / 2
-        return first_card_x
-    
-    def initialize_card_positions(self):
-        for i, card in enumerate(self.hand):
-            if i == 0:
-                card_location = self.set_card_on_center(card) 
-            else:
-                card_location = self.next_card_location(self.hand[i-1], overlap=50, vertical=(self.angle == 90 or self.angle == 270))
-            card.set_location(card_location)
-            
-    def next_card_location(self, previous_card, overlap=50, vertical=False):
-        card_rect = previous_card.get_rect()
-        if vertical:
-            new_y_coordinate = card_rect.y + card_rect.height - overlap
-            return (card_rect.x, new_y_coordinate)
-        else:
-            new_x_coordinate = card_rect.x + card_rect.width - overlap
-            return (new_x_coordinate, card_rect.y) 
-            
-    def set_card_on_center(self,card):
-        window_width, window_height = self.window.get_size()
-        image_width, image_height = card.get_size()
-        
-        # Center bottom
-        if self.angle == 0:
-            first_card_x = self.adjust_first_card_position(30)  # Assume overlap is 30 for player
-            return (first_card_x, window_height - image_height)
-        # Center right
-        elif self.angle == 90:
-            self.rotate_hand(90)   
-            return (window_width - image_width, window_height / 2 - image_height / 2)
-        # Center top
-        elif self.angle == 180:
-            self.rotate_hand(180)
-            return (window_width / 2 - image_width / 2, 0)
-        # Center left
-        elif self.angle == 270:
-            self.rotate_hand(270)
-            return (0, window_height / 2 - image_height / 2)
-    
-    def check_conditions(self, card, color, value):
-        if color == "":
-            return True
-        elif color == "black":
-            return True
-        elif card.get_color() == "black":
-            return True
-        elif card.get_color() == color:  
-            return True
-        elif card.get_value() == value: 
-            return True
-        return False
-
     
 class AIPlayer(Player):
     
