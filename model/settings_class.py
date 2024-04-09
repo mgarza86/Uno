4b1535c415fcaca6762caead976e62de30b529bd
Melanie Garza <mmg252@txstate.edu>
2024-04-08 19:45:35 -0500

    methods for settings class

--- model/settings_class.py
+++ model/settings_class.py
@@ -3,3 +3,12 @@ class Settings:
         self.sfx_enabled = True
         self.music_enabled = True
         self.difficulty = 'medium'
+        
+    def toggle_sfx(self):
+        self.sfx_enabled = not self.sfx_enabled
+
+    def toggle_music(self):
+        self.music_enabled = not self.music_enabled
+
+    def set_difficulty(self, difficulty):
+        self.difficulty = difficulty
